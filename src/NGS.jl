a33eee36de541191f03b8b762b56f32d886ff1ee
Gnanavel <gnanavel.mutharasu@tut.fi>
2016-02-16 12:42:25 +0200

    modified:   src/NGS.jl 	new file:   src/vcf.jl

--- src/NGS.jl
+++ src/NGS.jl
@@ -1,5 +1,22 @@
 module NGS
 
-# package code goes here
-
-end # module
+	# dependaencies
+	using Base
+	using GZip
+	
+	# Exported methods
+	export read,
+			records,
+			infos,
+			version,
+			header,
+			chromosomes,
+			fetch,
+			chrange,
+			region,
+			@vcf_recrd
+	
+	# Load package files
+	include(Pkg.dir("NGS", "src", "vcf.jl"))
+	
+end # NGS module
