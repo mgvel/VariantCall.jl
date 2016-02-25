288254df97b5ea895e8701c7c132587a2d48f7f1
Gnanavel <gnanavel.mutharasu@tut.fi>
2016-02-25 14:35:24 +0200

    modified:   src/recall.jl 	modified:   .gitignore

--- src/recall.jl
+++ src/recall.jl
@@ -1,26 +1,29 @@
 #!/usr/bin/env julia
 
 include("read.jl")
-
-function recall(paths = ARGS[1:end], min_alt_read::Int=4, min_alt_frac=0.2)
-   for file in paths
-     vcf = read(file)
+# Function to call variation based on given alt reads and fraction 
+function recall(paths = ARGS[1:end], min_alt_read::Int=2, min_alt_frac=0.05)
+	for file in paths
+		vcf = read(file)
       
-      for line in vcf
-        if ismatch(r"^#|CHROM", line)
-          println(line)
-        else
-          cols = split(line, "\t")
-          gt_reads  = []
-          alt_reads = []
-        
-          for gt in cols[5:end]
-            push!(gt_reads, split(gt, ':')[1])
-            push!(alt_reads, split(gt, ':')[2])
-          end
-          println(gt_reads)  
-        end
-      end
+		for line in vcf
+			if ismatch(r"^#|CHROM", line)
+				print(line)
+			else
+				cols = split(line, "\t")
+				alt_reads  = []
+				tot_reads = []
+				for gt in cols[5:end]
+					push!(alt_reads, parse(Int, split(gt, ':')[1]))
+					push!(tot_reads, parse(Int, split(gt, ':')[2]))
+				end
+				gt = maximum(alt_reads)
+				af = maximum(alt_reads ./ tot_reads)
+				if gt >= min_alt_read && af >= min_alt_frac
+					print(line)
+				end
+			end
+		end
    end
 end
 
