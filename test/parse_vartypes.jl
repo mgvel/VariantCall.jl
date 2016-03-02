974b73d6dcec52055d1f0ee9edda206491c05ad5
Gnanavel <gnanavel.mutharasu@tut.fi>
2016-03-02 15:54:10 +0200

    deleted:    test/chr1.ncv.stat 	deleted:    test/chr10.ncv.stat 	deleted:    test/chr11.ncv.stat 	deleted:    test/chr12.ncv.stat 	deleted:    test/chr13.ncv.stat 	deleted:    test/chr14.ncv.stat 	deleted:    test/chr15.ncv.stat 	deleted:    test/chr16.ncv.stat 	deleted:    test/chr17.ncv.stat 	deleted:    test/chr18.ncv.stat 	deleted:    test/chr19.ncv.stat 	deleted:    test/chr2.ncv.stat 	deleted:    test/chr20.ncv.stat 	deleted:    test/chr21.ncv.stat 	deleted:    test/chr22.ncv.stat 	deleted:    test/chr3.ncv.stat 	deleted:    test/chr4.ncv.stat 	deleted:    test/chr5.ncv.stat 	deleted:    test/chr6.ncv.stat 	deleted:    test/chr7.ncv.stat 	deleted:    test/chr8.ncv.stat 	deleted:    test/chr9.ncv.stat 	deleted:    test/chrX.ncv.stat 	deleted:    test/chrY.ncv.stat 	new file:   test/parse_vartypes.jl 	deleted:    test/variant_types.txt

---
+++ test/parse_vartypes.jl
@@ -0,0 +1,54 @@
+#!/usr/bin/env julia
+
+# Complete by Wed Mar  2 15:50:53 EET 2016
+# Program from parsing Ensembl gene ids for matching variant types
+# 13 variant types from test/variant_types.txt and
+# test/chr*.vep files are used as input
+
+# Reading 13 variant types
+function getlist(fh = ARGS[1])
+    list = readlines(open(fh))
+    return list
+end
+
+# Reading all the lines from inut .vep files
+function reads(files = ARGS[2:end])
+    lines = []
+    for file in files
+        println("Reading file ... $file")
+        fh = open(file)
+        line = readlines(fh)
+        append!(lines, line)
+        close(fh)
+    end
+    return lines
+end
+
+# Parsing the matching types from vep files
+# and writing into corresponding  .gnid files
+function parse(list, lines)
+    for var in list
+        genid = []
+        var = chomp(var)
+        outname = "$var.gnid"
+        outf = open(outname, "w")
+        for line in lines
+            if ismatch(r"^#", line)
+                print()
+            else
+                cols = split(line, '\t')
+                if contains(cols[7], var)
+                    gene = cols[4]*"\n"
+                    push!(genid, gene)
+                end
+            end
+        end
+        genid = unique(genid)
+        println("writing .... $outf")
+        write(outf, genid, "\n")
+    end
+end
+
+list  = getlist()
+lines = reads()
+parse(list, lines)
