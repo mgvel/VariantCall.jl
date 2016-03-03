7ef17f41524d920de87e8317f405e887a2ab1659
Gnanavel <gnanavel.mutharasu@tut.fi>
2016-03-03 16:35:40 +0200

    new file:   src/localize_genome.jl

---
+++ src/localize_genome.jl
@@ -0,0 +1,17 @@
+#!/usr/bin/env julia
+
+function readchr(fh=ARGS[1])
+    lines = readall(fh)
+    return lines
+end
+
+#=
+function index()
+end
+
+function coding()
+end
+
+function nonCoding()
+end
+=#
