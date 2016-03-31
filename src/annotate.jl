8ee1297c982a728acfae57e21117d50db812fd24
mgvel <velan.bio@gmail.com>
2016-03-31 14:47:43 +0300

    modified:   src/annotate.jl

--- src/annotate.jl
+++ src/annotate.jl
@@ -1,4 +1,10 @@
 
 function annotate(vcf_path, method, dbs,)
-    
+    shell('table_annovar.pl anno_tmp.vcf ~/tools/annovar-170715/humandb '
+            '-buildver hg19 --remove --otherinfo --outfile annotated '
+            '-operation g,f,f,f '
+            '-protocol refGene,cosmic70,1000g2014oct_all,esp6500si_alltable_annovar.pl anno_tmp.vcf ~/tools/annovar-170715/humandb '
+            '-buildver hg19 --remove --otherinfo --outfile annotated '
+            '-operation g,f,f,f '
+            '-protocol refGene,cosmic70,1000g2014oct_all,esp6500si_all')
 end
