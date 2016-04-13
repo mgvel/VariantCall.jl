a4ceb6c99749f001cc8a9ba9ae8e5802a980cc67
Gnanavel Mutharasu <velan.bio@gmail.com>
2016-04-13 11:15:29 +0300

    modified:   src/localize_genome.jl 	new file:   test/9000001-10000000

--- src/localize_genome.jl
+++ src/localize_genome.jl
@@ -1,9 +1,15 @@
 #!/usr/bin/env julia
+# Minimal annotation about the genomic postion
+# Try to classifiy genomic position at single neucleotide resolution
+# Using EnsEMBL vep libraries
 
 using GZip
 
 include("read.jl")
 
+"""
+Read entire chromosome from input file and return the lines
+"""
 function readchr(fh=ARGS[1])
     if ismatch(r".gz$", fh)
         f = GZip.open(fh)
@@ -25,12 +31,19 @@ function index(lines=readchr())
     return pos
 end
 
+"""
 out = index()
 for i in out
     println(i)
 end
 
 function coding()
+end
+"""
+test = readchr()
+for i in test
+    chomp(i)
+    println(i)
 end
 
 #=
