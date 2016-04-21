45ed0e56a1f4bc00318d10e2dc181a1b43620f52
Gnanavel Mutharasu <velan.bio@gmail.com>
2016-04-21 10:30:00 +0300

    new file:   src/fixheader.jl

---
+++ src/fixheader.jl
@@ -0,0 +1,20 @@
+#!/usr/bin/env julia
+
+function fixHeader(vcf=ARGS[1])
+    file = open(vcf)
+    lines = readlines(file)
+    info = "##fileformat=VCFv4.1
+##fileDate=20151202
+##source=Variant Calling
+##reference=/wrk/annalam/organisms/homo_sapiens/hg19_nochr.fa
+##INFO=Juliette
+##INFO=--alt-reads=2 --alt-frac=0.05"
+    println(info)
+    header = "#" * shift!(lines)
+    println(header)
+    for i in lines#[1:5]
+        println(chomp(i))
+    end
+end
+
+fixHeader()
