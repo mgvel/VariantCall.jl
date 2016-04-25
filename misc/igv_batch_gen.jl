e61b02adbdc72304ae2292de9df4dd448b0192cc
Gnanavel Mutharasu <velan.bio@gmail.com>
2016-04-25 17:35:22 +0300

    modified:   misc/igv_batch_gen.jl

--- misc/igv_batch_gen.jl
+++ misc/igv_batch_gen.jl
@@ -1,14 +1,29 @@
 #!/usr/bin/env julia
 
 path = "http://localhost:8000/bam/"
-bamList = readall(pipeline(`curl $path`, `awk '/.bam$/ {print $NF}'`))
+list = readall(pipeline(`curl $path`, `awk '/.bam$/ {print $NF}'`))
 
-list = split(bamList, '\n')
+fH = ARGS[1]
+wig = split(readall(fH), '\n')
 
-println(length(list)-1, " BAM files found!")
+bamList = split(list, '\n')
 
+println(length(bamList)-1, " BAM files found!")
+println(length(wig))
 
-for i in list[1:end-1]
+for f in wig
+    println(f)
+end
+
+for i in bamList[1:end-1]
+    for file in wig
+        ln = open(file)
+        lines = readall(ln)
+        chr = split(file, '.')
+        println(i, '\t', chr[1])
+    end
+end
+        #=
     println("new
 genome hg19
 load http://localhost:8000/bam/$i
@@ -17,3 +32,4 @@ goto chr18:27,984,924-27,984,924
 sort position
 snapshot")
 end
+=#
