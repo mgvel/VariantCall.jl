dcdacf744a719258cf84bffc015bcdf978348adc
Gnanavel Mutharasu <velan.bio@gmail.com>
2016-04-28 14:36:29 +0300

    new file:   misc/__pycache__/docopt.cpython-35.pyc 	modified:   misc/docopt.py 	new file:   misc/docopt.pyc 	modified:   misc/igv_batchFromVEP.jl 	modified:   misc/snp_position2wig.py 	new file:   src/vcf2wig.jl

--- src/vcf2wig.jl
+++ src/vcf2wig.jl
@@ -1,37 +1,33 @@
-#!/bin/env python
+#!/usr/bin/env julia
+
 """
 a tool for creating wig files from results of variant heterozygous bases.
-
-Usage:
-  snp_position2wig.py no_allele_fraction <het_snp_file>
-  snp_position2wig.py with_allele_fraction <het_snp_file>
-
-Options:
-  -h --help             Show this screen
-
-Author: Ebrahim Afyounian <ebrahim.afyounian@staff.uta.fi>
 """
-import docopt
+include("read.jl")
 
-def snp_position2wig_no_allele(snp_file_path):
-    h_file = open(snp_file_path, 'r')
-    header = next(h_file)
-    print('track graphType=bar windowingFunction=none autoScale=off viewLimits=0:1')
+function vcf2wig(vcf_path= ARGS[1])
+    lines = read(vcf_path)
+    println('track graphType=bar windowingFunction=none autoScale=off viewLimits=0:1')
     chrom = ''
     value = 1
-    for line in h_file:
-        cols = line.rstrip('\n').split('\t')
-        chr = cols[0]
-        pos = cols[1]
-        if chrom != chr:
+    for line in lines
+        cols = split(chomp(line),'\t')
+        chr = cols[1]
+        pos = cols[2]
+        if chrom != chr
             chrom = chr
-            print("variableStep chrom=%s" %chrom)
-            print("%s\t%s" %(pos, value))
-        else:
-            print( "%s\t%s" %(pos, value))
-    h_file.close()
+            println("variableStep chrom=$chrom")
+            println("$pos\t$value")
+        else
+            println("$pos\t$value")
+        end
+    end
+end
 
-def snp_position2wig_with_allele(snp_file_path):
+vcf2wig()
+
+#=
+function snp2wig_with_allele(vcf_path):
     h_file = open(snp_file_path, 'r')
     header = next(h_file)
     print( 'track graphType=bar windowingFunction=none autoScale=off viewLimits=0:1')
@@ -47,12 +43,17 @@ def snp_position2wig_with_allele(snp_file_path):
             print( "%s\t%s" %(pos, value))
         else:
             print( "%s\t%s" %(pos, value))
+        end
+    end
     h_file.close()
+end
 
-
+"""
 if __name__ == '__main__':
     args = docopt.docopt(__doc__)
     if args['no_allele_fraction']:
         snp_position2wig_no_allele(args['<het_snp_file>'])
     elif args['with_allele_fraction']:
         snp_position2wig_with_allele(args['<het_snp_file>'])
+"""
+=#
