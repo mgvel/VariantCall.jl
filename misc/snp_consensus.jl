3cbce584d21fc252ec57afbd7dbafc646833e12e
Gnanavel Mutharasu <velan.bio@gmail.com>
2016-05-03 16:18:55 +0300

    modified:   misc/snp_consensus.jl

--- misc/snp_consensus.jl
+++ misc/snp_consensus.jl
@@ -5,22 +5,35 @@ chrf  = ARGS[2]
 
 include("../src/read.jl")
 
-size = open(sizef)
-sizes = readlines(size)
-
-chrom = length = []
-for size in sizes
-    cols = split(size)
-    push!(chrom, cols[1])
-    push!(chrom, cols[2])
+function sizes(sizef)
+    sizes = readlines(open(sizef))
+    sizeD = Dict{ASCIIString,Int64}()
+    for size in sizes
+        cols = split(size, '\t')
+        sizeD[cols[1]] = parse(Int64, chomp(cols[2]))
+    end
+    return sizeD
 end
 
-sizes = Dict{ASCIIString,Int64}()
 
-lines = read(chrf)
+function chrArray(sizef, chrf)
+    sizeD = sizes(sizef)
+    lines = read(chrf)
+    names = split(lines[1])[5:end]
+    chr  = split(lines[end])[1]
+    array = "chr" *  chr
+    array = zeros(sizeD[chr])
+    return array
 
-samples = split(lines[1])[5:end]
-chr =   split(lines[end])[1]
+end
+array = chrArray(sizef, chrf)
+println(length(array))
 
+for v in array[1:10]
+    println(v)
+end
+
+#=
 println(length(samples))
 println(chr)
+=#
