abef7bf219ff9b98fa3f49a477601e5fe94e025b
Gnanavel Mutharasu <velan.bio@gmail.com>
2016-05-03 17:40:38 +0300

    modified:   misc/snp_consensus.jl

--- misc/snp_consensus.jl
+++ misc/snp_consensus.jl
@@ -19,19 +19,33 @@ end
 function chrArray(sizef, chrf)
     sizeD = sizes(sizef)
     lines = read(chrf)
+    data = lines[2:end]
     names = split(lines[1])[5:end]
     chr  = split(lines[end])[1]
     array = "chr" *  chr
     array = zeros(sizeD[chr])
-    return array
+    return array, names, data
 
 end
-array = chrArray(sizef, chrf)
-println(length(array))
 
-for v in array[1:10]
-    println(v)
+array, samples, data = chrArray(sizef, chrf)
+println(length(samples), length(split(data[1])[5:end]))
+
+matrix = Array{Int}(length(array), length(samples))
+
+for pos in array[1:10]
+    sample = []
+    for del in split(data[1])[5:end]
+        alts = parse(Int64, split(del, ':')[1])
+        if alts >= 2                               # Number of alternative reads
+            push!(sample, 1)
+        else
+            push!(sample, 0)
+        end
+    end
+    matrix[pos] = sample
 end
+println(ndims(matrix))
 
 #=
 println(length(samples))
