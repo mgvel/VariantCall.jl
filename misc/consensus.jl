139301c78e77187be8b4fd3d1500a61f81c5f018
Gnanavel Mutharasu <gnanavel.mutharasu@gmail.com>
2016-05-05 18:53:55 +0300

    modified:   misc/consensus.jl

--- misc/consensus.jl
+++ misc/consensus.jl
@@ -3,13 +3,12 @@
 """
 Consensus checking for variant positions across samples
 """
-file = ARGS[1] # vcf /vcf.gz file
-
+file = ARGS[1] # .vcf/.vcf.gz file
 include("../src/read.jl")
 
 vcf = read(file)
 header = []
-names = split(vcf[1])[5:end]  # assumes first line as header
+names = split(vcf[1])[5:end]  # assumes the first line as header
 push!(header, "Position")
 push!(header, "Consensus")
 append!(header, names)
@@ -23,7 +22,6 @@ for line in vcf[2:end]
 	arr =[]
 	cols = split(line)
 	print(cols[2], '\t')
-	#push!(arr, cols[2])
 	alts = []
 	for sample in cols[5:end]
 		altRead = parse(Int64, split(sample, ':')[1])
@@ -35,7 +33,6 @@ for line in vcf[2:end]
 	end
 	consensus = (sum(alts)/length(alts))*100
 	@printf("%6.3f", consensus)
-	#append!(arr, alts)
 	for i in alts
 		print('\t', i)
 	end
