7c669456c3aaae3c14dcc78b644ea1794111664f
Gnanavel Mutharasu <gnanavel.mutharasu@gmail.com>
2016-05-20 13:53:39 +0300

    modified:   misc/filter_igvIMG.jl

--- misc/filter_igvIMG.jl
+++ misc/filter_igvIMG.jl
@@ -1,9 +1,37 @@
 #!/usr/bin/env julia
 
-pnglist = readdir()
+pnglist = filter!(r"\.png", readdir())
 
-println(length(pnglist))
-
-for i in pnglist
-	println(i)
+function expand(locci)
+    list = []
+    pos = split(locci, ':')
+        chr = pos[1]
+        region = split(pos[2], '-')
+        n = parse(Int64, region[1])
+        while n <= parse(Int64, region[end])
+             out = "$chr" * ":" * "$n"
+            push!(list, out)
+            n +=  1
+        end
+    return list
 end
+
+function getSNPs(pnglist=pnglist)
+	snps = []
+	for img in pnglist
+		img = replace(img, ".png", "")
+		parts = split(img, '\_')
+		chr = replace(parts[1], "chr", "")
+		start = parse(Int, replace(parts[2], ",", ""))
+		endp  = parse(Int, replace(parts[3], ",", ""))
+		pos = "$chr" * ":" * "$start" * "-" * "$endp"
+		vars = expand(pos)
+		append!(snps, vars)
+	end
+	snps = unique(snps)
+	for snp in snps
+		println(snp)
+	end
+end
+
+getSNPs()
